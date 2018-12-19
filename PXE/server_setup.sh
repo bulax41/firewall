16d4f44ebef020c50e09758ec94b4ffd91079ddd
Peter Johnson <peter@johnsonhome.org>
2018-12-19 12:01:41 -0600

    Updated server_setup.sh

--- PXE/server_setup.sh
+++ PXE/server_setup.sh
@@ -32,14 +32,14 @@ ONTIMEOUT local
 menu title ########## PXE Boot Menu ##########
 
 label 1
-menu label ^1) Install CentOS 7 x64 with Local Repo
+menu label ^1)  CentOS 7 x64
 kernel centos7/vmlinuz
-append initrd=centos7/initrd.img method=ftp://10.70.70.254/pub ks=ftp://10.70.70.254/pub/centos7.cfg devfs=nomount
+append initrd=centos7/initrd.img method=ftp://$PREFIX.254/pub ks=ftp://$PREFIX/pub/centos7.cfg devfs=nomount
 
 label 2
-menu label ^2) Install CentOS 7 x64 with http://mirror.centos.org Repo
+menu label ^2) Firewall
 kernel centos7/vmlinuz
-append initrd=centos7/initrd.img method=http://mirror.centos.org/centos/7/os/x86_64/ devfs=nomount ip=dhcp
+append initrd=centos7/initrd.img method=ftp://$PREFIX.254/pub ks=ftp://$PREFIX/pub/firewall.cfg devfs=nomount
 
 END
 
@@ -77,6 +77,56 @@ autopart --type lvm --fstype xfs --nohome
 @^minimal
 @core
 %end
+%addon com_redhat_kdump --disable --reserve-mb='auto'
+%end
+END
+
+cat > /var/ftp/pub/firewall.cfg << END
+#platform=x86, AMD64, or Intel EM64T
+#version=DEVEL
+# Firewall configuration
+firewall --disabled
+# Install OS instead of upgrade
+install
+# Use FTP installation media
+url --url="ftp://10.70.70.254/pub/"
+# Root password
+rootpw --plaintext VDIware123
+# System authorization information
+auth useshadow passalgo=sha512
+# Use graphical install
+graphical
+firstboot disable
+# System keyboard
+keyboard us
+# System language
+lang en_US
+# SELinux configuration
+selinux enable
+# Installation logging level
+logging level=info
+# System timezone
+timezone America/Chicago
+# System bootloader configuration
+bootloader location=mbr
+clearpart --all --initlabel
+autopart --type lvm --fstype xfs --nohome
+%packages
+@^minimal
+@core
+%end
+%post --log=/root/post.log
+yum -y install epel-release
+yum -y install ipset ipset-service wireshark zip ntp python2-pip strongswan openvpn easy-rsa iptables-services net-snmp net-tools quagga  sysstat traceroute telnet  policycoreutils-python bridge-utils libsemanage-python ipa-client nmap wget
+pip install --upgrade pip
+pip install python-telegram-bot --upgrade
+pip install configparser --upgrade
+wget ftp://$PREFIX.254/pub/VMwareTools-10.2.5-8068406.tar.gz
+tar zxf VMwareTools-10.2.5-8068406.tar.gz
+cd vmware-tools-distrib
+./vmware-install.pl -d -f
+%end
+
 %addon com_redhat_kdump --disable --reserve-mb='auto'
 %end
 END
